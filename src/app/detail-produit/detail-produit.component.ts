219b83ae4048acc8c646e6526228c95cfa6b7025
mamebassine <mamebassine06@gmail.com>
2024-07-26 01:13:12 +0000

    Affichage des produits.

--- src/app/detail-produit/detail-produit.component.ts
+++ src/app/detail-produit/detail-produit.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+// // src/app/detail-produit/detail-produit.component.ts
+
+// import { Component, Input } from '@angular/core';
+
+// @Component({
+//   selector: 'app-detail-produit',
+//   templateUrl: './detail-produit.component.html',
+//   styleUrls: ['./detail-produit.component.css']
+// })
+// export class DetailProduitComponent {
+//   @Input() produit: any;
+// 1}
+
+
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detail-produit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detail-produit.component.html',
-  styleUrl: './detail-produit.component.css'
+  styleUrls: ['./detail-produit.component.css']
 })
 export class DetailProduitComponent {
-
+  @Input() produit: any;
 }
